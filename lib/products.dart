252483c5524f0af6927c1b4095fb8f270f180485
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-15 15:29:26 -0500

    Add buildProductItem method

--- lib/products.dart
+++ lib/products.dart
@@ -5,17 +5,22 @@ class Products extends StatelessWidget {
 
 	Products([this._products = const []]);
 
+	Widget _buildProductItem(BuildContext context, int index) {
+		return Card(
+			child: Column(
+				children: <Widget>[
+					Image.asset('assets/food.jpg'),
+					Text(_products[index]),
+				],
+			),
+		);
+	}
+
 	@override
 	Widget build(BuildContext context) {
-		return ListView(
-			children: _products.map((element) => Card(
-				child: Column(
-					children: <Widget>[
-						Image.asset('assets/food.jpg'),
-						Text(element),
-					],
-				),
-			)).toList(),
+		return ListView.builder(
+			itemBuilder: _buildProductItem,
+			itemCount: _products.length,
 		);
 	}
 }
