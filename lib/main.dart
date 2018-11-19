9378f68e990c6a0d17fa6681dddb92f8a4e09541
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-19 13:41:48 -0500

    Manage product state in main widge

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 import './pages/auth.dart';
+import './pages/products_admin.dart';
+import './pages/products.dart';
+import './pages/product.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+	@override
+	State<StatefulWidget> createState() {
+		return _MyAppState();
+	}
+}
+
+class _MyAppState extends State<MyApp> {
+	List<Map<String, dynamic>> _products = [];
+
+	void _addProduct(Map<String, dynamic> product) {
+		setState(() {
+			_products.add(product);
+		});
+	}
+
+	void _deleteProduct(int index) {
+		setState(() {
+			_products.removeAt(index);
+		});
+	}
 
 	@override
 	Widget build(BuildContext context) {
@@ -13,7 +37,43 @@ class MyApp extends StatelessWidget {
 				primarySwatch: Colors.teal,
 				accentColor: Colors.yellowAccent,
 			),
-			home: AuthPage(),
+			routes: {
+				// The '/' is a special route that Flutter uses to show as the 'home' screen.
+				// you CANNOT use both the home: property in MaterialApp as well as this '/' route
+				// or it will throw an error. Use one or the other.
+				'/': (BuildContext context) => ProductsPage(_products),
+				'/admin': (BuildContext context) => ProductsAdminPage(_addProduct, _deleteProduct),
+			},
+			// This property allows us to use 'dynamic' named routes. Think of them as '/route/:id'
+			// These routes are supplemental to the routes property/values as any routes that are hit
+			// that aren't in routes, go to this function
+			onGenerateRoute: (RouteSettings settings) {
+				final List<String> pathElements = settings.name.split('/');
+
+				if (pathElements[0] != '') {
+					return null;
+				}
+
+				if (pathElements[1] == 'product') {
+					final int index = int.parse(pathElements[2]);
+
+					return MaterialPageRoute<bool>(
+						builder: (BuildContext context) => ProductPage(
+							_products[index]['title'],
+							_products[index]['image']
+						),
+					);
+				}
+
+				return null;
+			},
+			// Executes whenever onGenerateRoute fails to generate a route which will happen if
+			// the "null" is returned from onGenerateRoute.
+			onUnknownRoute: (RouteSettings settings) {
+				return MaterialPageRoute(
+					builder: (BuildContext context) => ProductsPage(_products),
+				);
+			},
 		);
 	}
 }
