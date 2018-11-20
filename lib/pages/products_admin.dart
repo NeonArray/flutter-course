a63a3a043059b02c5f93c7755f6962328f58be91
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-20 11:34:47 -0500

    Refactor drawer widget into buildDrawer method

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -9,25 +9,30 @@ class ProductsAdminPage extends StatelessWidget {
 
 	ProductsAdminPage(this.addProduct, this.deleteProduct);
 
+	Drawer _buildDrawer(BuildContext context) {
+		return Drawer(
+			child: Column(
+				children: <Widget>[
+					AppBar(
+						automaticallyImplyLeading: false,
+						title: Text('Choose'),
+					),
+					ListTile(
+						leading: Icon(Icons.shop),
+						title: Text('All Products'),
+						onTap: () => Navigator.pushReplacementNamed(context, '/'),
+					),
+				],
+			),
+		);
+	}
+
 	@override
 	Widget build(BuildContext context) {
 		return DefaultTabController(
 			length: 2,
 			child: Scaffold(
-				drawer: Drawer(
-					child: Column(
-						children: <Widget>[
-							AppBar(
-								automaticallyImplyLeading: false,
-								title: Text('Choose'),
-							),
-							ListTile(
-								title: Text('All Products'),
-								onTap: () => Navigator.pushReplacementNamed(context, '/'),
-							),
-						],
-					),
-				),
+				drawer: _buildDrawer(context),
 				appBar: AppBar(
 					title: Text('Manage Products'),
 					bottom: TabBar(
