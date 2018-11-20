330c88d1809f429d7ac2a484d627ad6b06ebd0cb
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-20 11:35:38 -0500

    Refactor text field widgets into separate methods

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,8 +1,67 @@
 import 'package:flutter/material.dart';
 
-import './products.dart';
+class AuthPage extends StatefulWidget {
+	@override
+	_AuthPageState createState() => new _AuthPageState();
+}
 
-class AuthPage extends StatelessWidget {
+class _AuthPageState extends State<AuthPage> {
+	String _emailValue;
+	String _passwordValue;
+	bool _acceptTerms = false;
+
+	DecorationImage _buildBackgroundImage() {
+		return DecorationImage(
+			fit: BoxFit.cover,
+			colorFilter: ColorFilter.mode(
+				Colors.black.withOpacity(0.5),
+				BlendMode.dstATop,
+			),
+			image: AssetImage('assets/background.jpg'),
+		);
+	}
+
+	Widget _buildEmailTextField() {
+		return TextField(
+			keyboardType: TextInputType.emailAddress,
+			decoration: InputDecoration(
+				labelText: 'E-Mail',
+				filled: true,
+				fillColor: Colors.white,
+			),
+			onChanged: (String value) => setState(() => _emailValue = value),
+		);
+	}
+
+	Widget _buildPasswordTextField () {
+		return TextField(
+			obscureText: true,
+			decoration: InputDecoration(
+				labelText: 'Password',
+				filled: true,
+				fillColor: Colors.white,
+			),
+			onChanged: (String value) => setState(() => _passwordValue = value),
+		);
+	}
+
+	SwitchListTile _buildAcceptSwitchTile() {
+		return SwitchListTile(
+			value: _acceptTerms,
+			onChanged: (bool value) {
+				setState(() => _acceptTerms = value);
+			},
+			title: Text('Accept Terms'),
+		);
+	}
+
+	Widget _buildLoginButton() {
+		return RaisedButton(
+			color: Theme.of(context).primaryColor,
+			child: Text('LOGIN'),
+			onPressed: () => Navigator.pushReplacementNamed(context, '/products'),
+		);
+	}
 
 	@override
 	Widget build(BuildContext context) {
@@ -10,10 +69,28 @@ class AuthPage extends StatelessWidget {
 			appBar: AppBar(
 				title: Text('Login'),
 			),
-			body: Center(
-				child: RaisedButton(
-					child: Text('LOGIN'),
-					onPressed: () => Navigator.pushReplacementNamed(context, '/'),
+			body: Container(
+				decoration: BoxDecoration(
+					image: _buildBackgroundImage(),
+				),
+				padding: EdgeInsets.all(10.0),
+				child: Center(
+					child: SingleChildScrollView(
+						child: Column(
+							children: <Widget>[
+								_buildEmailTextField(),
+								SizedBox(
+									height: 10.0,
+								),
+								_buildPasswordTextField(),
+								_buildAcceptSwitchTile(),
+								SizedBox(
+									height: 30.0,
+								),
+								_buildLoginButton(),
+							],
+						),
+					),
 				),
 			),
 		);
