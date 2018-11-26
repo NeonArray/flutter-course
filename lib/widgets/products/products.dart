2f06a2514fcd169cdf647db26a27887f703d850d
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-26 09:13:33 -0500

    Refactor class to use ScopedModelDescendant

--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
+import 'package:flutter_course/models/product.dart';
 import 'package:flutter_course/widgets/products/product_card.dart';
+import 'package:flutter_course/scoped_models/products.dart';
 
 class Products extends StatelessWidget {
-	final List<Map<String, dynamic>> _products;
-
-	Products(this._products);
 
 	/// Returns a ListView widget that contains Cards for each product.
-	Widget _buildProductList() {
+	Widget _buildProductList(List<Product> products) {
 		Widget productCards = Center(child: Text('No products found'));
 
-		if (_products.length > 0) {
+		if (products.length > 0) {
 			productCards = ListView.builder(
-				itemBuilder: (BuildContext context, int index) => ProductCard(_products[index], index),
-				itemCount: _products.length,
+				itemBuilder: (BuildContext context, int index) => ProductCard(products[index], index),
+				itemCount: products.length,
 			);
 		} else {
 			productCards = Container();
@@ -25,6 +25,10 @@ class Products extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context) {
-		return _buildProductList();
+		return ScopedModelDescendant<ProductsModel>(
+			builder: (BuildContext context, Widget child, ProductsModel model) {
+				return _buildProductList(model.products);
+			},
+		);
 	}
 }
