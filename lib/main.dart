8a59bf0cd257327b8d7d43286445dc338ae6569c
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-26 09:15:17 -0500

    Remove internal passing of state with scoped model

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-import './pages/auth.dart';
-import './pages/products_admin.dart';
-import './pages/products.dart';
-import './pages/product.dart';
+import 'package:flutter_course/scoped_models/products.dart';
+import 'package:flutter_course/pages/auth.dart';
+import 'package:flutter_course/pages/products_admin.dart';
+import 'package:flutter_course/pages/products.dart';
+import 'package:flutter_course/pages/product.dart';
 
-void main() {
-//	debugPaintSizeEnabled = true;
-	runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
 	@override
@@ -17,69 +15,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-	List<Map<String, dynamic>> _products = [];
-
-	void _addProduct(Map<String, dynamic> product) {
-		setState(() {
-			_products.add(product);
-		});
-	}
-
-	void _deleteProduct(int index) {
-		setState(() {
-			_products.removeAt(index);
-		});
-	}
 
 	@override
 	Widget build(BuildContext context) {
-		return MaterialApp(
-			theme: ThemeData(
-				brightness: Brightness.light,
-				primarySwatch: Colors.teal,
-				accentColor: Colors.grey,
-				buttonColor: Colors.deepPurple,
-			),
-			routes: {
-				// The '/' is a special route that Flutter uses to show as the 'home' screen.
-				// you CANNOT use both the home: property in MaterialApp as well as this '/' route
-				// or it will throw an error. Use one or the other.
-				'/': (BuildContext context) => AuthPage(),
-				'/products': (BuildContext context) => ProductsPage(_products),
-				'/admin': (BuildContext context) => ProductsAdminPage(_addProduct, _deleteProduct),
-			},
-			// This property allows us to use 'dynamic' named routes. Think of them as '/route/:id'
-			// These routes are supplemental to the routes property/values as any routes that are hit
-			// that aren't in routes, go to this function
-			onGenerateRoute: (RouteSettings settings) {
-				final List<String> pathElements = settings.name.split('/');
+		return ScopedModel<ProductsModel>(
+			model: ProductsModel(),
+			child: MaterialApp(
+				theme: ThemeData(
+					brightness: Brightness.light,
+					primarySwatch: Colors.teal,
+					accentColor: Colors.grey,
+					buttonColor: Colors.deepPurple,
+				),
+				routes: {
+					// The '/' is a special route that Flutter uses to show as the 'home' screen.
+					// you CANNOT use both the home: property in MaterialApp as well as this '/' route
+					// or it will throw an error. Use one or the other.
+					'/': (BuildContext context) => AuthPage(),
+					'/products': (BuildContext context) => ProductsPage(),
+					'/admin': (BuildContext context) => ProductsAdminPage(null, null, null, null),
+				},
+				// This property allows us to use 'dynamic' named routes. Think of them as '/route/:id'
+				// These routes are supplemental to the routes property/values as any routes that are hit
+				// that aren't in routes, go to this function
+				onGenerateRoute: (RouteSettings settings) {
+					final List<String> pathElements = settings.name.split('/');
 
-				if (pathElements[0] != '') {
+					if (pathElements[0] != '') {
+						return null;
+					}
+
+					if (pathElements[1] == 'product') {
+						final int index = int.parse(pathElements[2]);
+
+						return MaterialPageRoute<bool>(
+							builder: (BuildContext context) => ProductPage(),
+						);
+					}
+
 					return null;
-				}
-
-				if (pathElements[1] == 'product') {
-					final int index = int.parse(pathElements[2]);
-
-					return MaterialPageRoute<bool>(
-						builder: (BuildContext context) => ProductPage(
-							_products[index]['title'],
-							_products[index]['image'],
-							_products[index]['price'],
-							_products[index]['description'],
-						),
+				},
+				// Executes whenever onGenerateRoute fails to generate a route which will happen if
+				// the "null" is returned from onGenerateRoute.
+				onUnknownRoute: (RouteSettings settings) {
+					return MaterialPageRoute(
+						builder: (BuildContext context) => ProductsPage(),
 					);
-				}
-
-				return null;
-			},
-			// Executes whenever onGenerateRoute fails to generate a route which will happen if
-			// the "null" is returned from onGenerateRoute.
-			onUnknownRoute: (RouteSettings settings) {
-				return MaterialPageRoute(
-					builder: (BuildContext context) => ProductsPage(_products),
-				);
-			},
+				},
+			)
 		);
 	}
 }
--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_course/widgets/products/price_tag.dart';
-import '../ui/title_default.dart';
-import './address_tag.dart';
+import 'package:flutter_course/widgets/products/address_tag.dart';
+import 'package:flutter_course/widgets/ui/title_default.dart';
+import 'package:flutter_course/models/product.dart';
 
 class ProductCard extends StatelessWidget {
-	final Map<String, dynamic> product;
+	final Product product;
 	final int productIndex;
 
 	ProductCard(this.product, this.productIndex);
 
 	Widget _buildTitlePriceRow() {
 		return Container(
-				padding: EdgeInsets.all(10.0),
-				child: Row(
-					mainAxisAlignment: MainAxisAlignment.spaceBetween,
-					children: <Widget>[
-						Column(
-							crossAxisAlignment: CrossAxisAlignment.start,
-							children: <Widget>[
-								TitleDefault(product['title']),
-								AddressTag('Union Square, San Francisco'),
-							],
-						),
-						SizedBox(
-							width: 8.0,
-						),
-						PriceTag(product['price'].toString()),
-					],
-				)
+			padding: EdgeInsets.all(10.0),
+			child: Row(
+				mainAxisAlignment: MainAxisAlignment.spaceBetween,
+				children: <Widget>[
+					Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: <Widget>[
+							TitleDefault(product.title),
+							AddressTag('Union Square, San Francisco'),
+						],
+					),
+					SizedBox(
+						width: 8.0,
+					),
+					PriceTag(product.price.toString()),
+				],
+			)
 		);
 	}
 
@@ -40,8 +41,8 @@ class ProductCard extends StatelessWidget {
 					color: Colors.grey,
 					icon: Icon(Icons.info),
 					onPressed: () => Navigator.pushNamed<bool>(
-							context,
-							'/product/' + productIndex.toString()
+						context,
+						'/product/' + productIndex.toString()
 					),
 				),
 				IconButton(
@@ -58,7 +59,7 @@ class ProductCard extends StatelessWidget {
 		return Card(
 			child: Column(
 				children: <Widget>[
-					Image.asset(product['image']),
+					Image.asset(product.image),
 					_buildTitlePriceRow(),
 					_buildActionButtons(context),
 				],
