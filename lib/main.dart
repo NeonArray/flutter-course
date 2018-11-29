a07b9131acd29ef19a9939f05f8788a9b228bda6
Aaron Arney <aaron.g.arney@gmail.com>
2018-11-29 11:59:16 -0500

    Route to auth page if unauthenticated

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,37 @@ import 'package:flutter_course/pages/product.dart';
 import 'package:flutter_course/scoped_models/main.dart';
 import 'package:flutter_course/models/product.dart';
 
+
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatefulWidget {
 	@override
 	_MyAppState createState() => new _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
+	final MainModel _model = MainModel();
+	bool _isAuthenticated = false;
+
+
+	@override
+	void initState() {
+		_model.autoAuthenticate();
+		_model.userSubject.listen((bool isAuthenticated) {
+			setState(() {
+				_isAuthenticated = isAuthenticated;
+			});
+		});
+		super.initState();
+	}
+
 
 	@override
 	Widget build(BuildContext context) {
-		final MainModel model = MainModel();
-
 		return ScopedModel<MainModel>(
-			model: model,
+			model: _model,
 			child: MaterialApp(
 				theme: ThemeData(
 					brightness: Brightness.light,
@@ -34,14 +50,18 @@ class _MyAppState extends State<MyApp> {
 					// The '/' is a special route that Flutter uses to show as the 'home' screen.
 					// you CANNOT use both the home: property in MaterialApp as well as this '/' route
 					// or it will throw an error. Use one or the other.
-					'/': (BuildContext context) => AuthPage(),
-					'/products': (BuildContext context) => ProductsPage(model),
-					'/admin': (BuildContext context) => ProductsAdminPage(model),
+					'/': (BuildContext context) => !_isAuthenticated ? AuthPage() : ProductsPage(_model),
+					'/admin': (BuildContext context) => !_isAuthenticated ? AuthPage() : ProductsAdminPage(_model),
 				},
 				// This property allows us to use 'dynamic' named routes. Think of them as '/route/:id'
 				// These routes are supplemental to the routes property/values as any routes that are hit
 				// that aren't in routes, go to this function
 				onGenerateRoute: (RouteSettings settings) {
+					if (!_isAuthenticated) {
+						return MaterialPageRoute(
+							builder: (BuildContext context) => AuthPage(),
+						);
+					}
 					final List<String> pathElements = settings.name.split('/');
 
 					if (pathElements[0] != '') {
@@ -51,14 +71,14 @@ class _MyAppState extends State<MyApp> {
 					if (pathElements[1] == 'product') {
 						String productId = pathElements[2];
 
-						final Product product = model.allProducts.firstWhere((Product product) {
+						final Product product = _model.allProducts.firstWhere((Product product) {
 							return product.id == productId;
 						});
 
-						model.selectProduct(productId);
+						_model.selectProduct(productId);
 
 						return MaterialPageRoute<bool>(
-							builder: (BuildContext context) => ProductPage(product),
+							builder: (BuildContext context) => !_isAuthenticated ? AuthPage() : ProductPage(product),
 						);
 					}
 
@@ -68,7 +88,7 @@ class _MyAppState extends State<MyApp> {
 				// the "null" is returned from onGenerateRoute.
 				onUnknownRoute: (RouteSettings settings) {
 					return MaterialPageRoute(
-						builder: (BuildContext context) => ProductsPage(model),
+						builder: (BuildContext context) => !_isAuthenticated ? AuthPage() : ProductsPage(_model),
 					);
 				},
 			)
